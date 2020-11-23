aa2e4a07add4f53d6ec24152c5263ce8a9731009
Ali Nguyen <alin57717@gmail.com>
2020-11-22 21:35:11 -0800

    create associations to database tables

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreign_key: 'category_id'
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreign_key: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreign_key: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreign_key: 'tag_id'
+})
 
 module.exports = {
   Product,
